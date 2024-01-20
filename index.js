77e479e0ce2a483c520ce9a4906ee1b2fa614ff9
Sơn Nguyễn <106434538+1402sn@users.noreply.github.com>
2024-01-20 01:44:18 -0800

    index.js

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-///By Alan
-///By Alan
-const express = require("express");
-const app = express();
-app.listen(() => console.log("Server Started  A L A N"));
-
-app.use('/ping', (req, res) => {
-  res.send(new Date());
-});
-
+const keepAliveServer = require('./keep_alive.js');
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); // All partials are loaded automatically
-///By Alan
 client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!                        Fatures: 
-1- Streaming Added
-2- Joined Voice Channel
-3- Without Offline 24/7
-Dont Miss Credits To 'kurdm98`);
+  console.log(`${client.user.username} is ready!`);
 })
 const { joinVoiceChannel } = require('@discordjs/voice');
 client.on('ready', () => {
-///By Alan 
   client.user.setActivity("", { type: "", url: "" })
   setInterval(async () => {
     client.channels.fetch("1185234955161309295")
@@ -34,5 +19,3 @@ client.on('ready', () => {
   }, 1000)
 });
 client.login(process.env.token);
-const keepAliveServer = require('./keep_alive.js');
-///By Alan
